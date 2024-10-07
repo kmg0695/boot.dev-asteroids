e02f37437dec689858081818b1c4a355044266d5
79101566+kmg0695@users.noreply.github.com <79101566+kmg0695@users.noreply.github.com>
2024-10-06 21:14:29 -0400

    Finished Ch3-2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sys
+
 import pygame
 
-import asteroidfield
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import *
@@ -8,38 +9,46 @@ from player import Player
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    asteroidfield = AsteroidField()
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color="black")
-        for player in updatable:
-            player.update(dt)
-            asteroidfield.update(dt)
-        for player in drawable:
-            player.draw(screen)
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        delta = clock.tick(60)
-        dt = delta / 1000
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
